6d2350d42109f397b60236040ee695ee7873417b
Mujahid Ali <mdownali7@gmail.com>
2022-05-07 12:52:26 +0500

    database changes

---
+++ config/database.js
@@ -0,0 +1,30 @@
+ // strapi-api/config/database.js
+ module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "codingchalu"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "1439211"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
+
+
+
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
